25b5c28cfe34ffb2304e32bb7ebccfce1fc48312
Shashank Pachava <spachava753@gmail.com>
2019-06-18 21:36:29 -0400

    Add basic Spring Boot 2.1.4 Reference App

---
+++ src/main/java/net/thespblog/springbootref/HelloController.java
@@ -0,0 +1,14 @@
+package net.thespblog.springbootref;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Shashank Pachava!";
+    }
+
+}
