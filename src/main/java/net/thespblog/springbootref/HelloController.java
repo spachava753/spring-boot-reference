a2166621d12700c71c129af02fbf4edc7a75040b
Shashank Pachava <spachava753@gmail.com>
2019-07-13 20:13:58 -0400

    Change something

--- src/main/java/net/thespblog/springbootref/HelloController.java
+++ src/main/java/net/thespblog/springbootref/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Shashank Pachava!";
+        return "Greetings from Shashank Pachava and Lakshmi!";
     }
 
 }
